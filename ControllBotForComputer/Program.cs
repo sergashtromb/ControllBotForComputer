5ec069bea1842896d6dbcab05a8d86c9bc410d4f
sergioshtromb <81059697+sergashtromb@users.noreply.github.com>
2022-09-06 11:17:58 +0700

    Include

--- ControllBotForComputer/Program.cs
+++ ControllBotForComputer/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using Telegram.Bot;
+using System.IO;
+using Telegram.Bot.Types;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Threading;
+using File = System.IO.File;
 
-namespace ControllBotForComputer
+namespace MyBot
 {
+
+
+    public struct ChatBot
+    {
+
+        public string token;
+
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string fileData = File.ReadAllText("C:\\Users\\Zver\\Desktop\\data.json");
+
+            ChatBot chatBot = JsonConvert.DeserializeObject<ChatBot>(fileData);
+            var botClient = new TelegramBotClient(chatBot.token);
+
+            botClient.StartReceiving(Update, Error);
+
+
+            Console.ReadLine();
+        }
+
+        async private static Task Update(ITelegramBotClient client, Update update, CancellationToken arg3)
+        {
+            var message = update.Message;
+            if (message != null)
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, "Сообщение");
+            }
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
         }
     }
 }
