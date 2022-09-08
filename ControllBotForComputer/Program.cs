d9189dc82f5a4d71d3f1b8e6fcf27370bc064108
sergioshtromb <81059697+sergashtromb@users.noreply.github.com>
2022-09-08 10:26:02 +0700

    Commands

--- ControllBotForComputer/Program.cs
+++ ControllBotForComputer/Program.cs
@@ -25,12 +25,14 @@ namespace MyBot
 
     internal class Program
     {
+        static string fileData = File.ReadAllText("C:\\Users\\Zver\\Desktop\\data.json");
+        static ChatBot chatBot = JsonConvert.DeserializeObject<ChatBot>(fileData);
+
+        static string com = "";
         static async Task Main(string[] args)
         {
 
-            string fileData = File.ReadAllText("C:\\Users\\Zver\\Desktop\\data.json");
-
-            ChatBot chatBot = JsonConvert.DeserializeObject<ChatBot>(fileData);
+            
             var botClient = new TelegramBotClient(chatBot.token);
             
             botClient.StartReceiving(Update, Error);
@@ -41,19 +43,71 @@ namespace MyBot
 
         async private static Task Update(ITelegramBotClient client, Update update, CancellationToken arg3)
         {
+            
             var message = update.Message;
             if (message != null)
             {
-                if (message.Text == "Открыть папку")
+                if(com == "")
                 {
-                    string path = "C:\\Users\\Zver\\Documents";
-                    Process.Start(new ProcessStartInfo { FileName="explorer", Arguments= $"/n, /select, {path}" });
-                    Console.WriteLine("OK");
-                    
+                    if (message.Text == "Открыть папку")
+                    {
+
+                        await client.SendTextMessageAsync(message.Chat.Id, "Введите путь");
+                        com = "PathOpenDir";
+                        Console.WriteLine("com != \"\"");
+                    }
+                    else if (message.Text == "Узнать текущие процессы")
+                    {
+
+                        Process[] processes = Process.GetProcesses();
+                        string listProcesses = "";
+                        for (int i = 0; i < processes.Length; i++)
+                        {
+                            if (processes[i].MainWindowHandle != IntPtr.Zero)
+                            {
+                                listProcesses += "\n" + processes[i].ProcessName;
+                            }
+
+                        }
+                        await client.SendTextMessageAsync(message.Chat.Id, listProcesses);
+                        Console.WriteLine("OK");
+                    }
+                    else if (message.Text == "Узнать количество процессов")
+                    {
+                        Process[] processes = Process.GetProcesses();
+                        await client.SendTextMessageAsync(message.Chat.Id, processes.Length.ToString());
+                    }
+                    else
+                    {
+                        await client.SendTextMessageAsync(message.Chat.Id, message.Text);
+                    }
+                } 
+                else if(com == "PathOpenDir")
+                {
+                    if(message.Text != "")
+                    {
+                        Console.WriteLine("Path");
+                        string path = message.Text.Replace("/", "\\");
+                        Console.WriteLine(path);
+                        try
+                        {
+                            Process.Start(new ProcessStartInfo { FileName = "explorer", Arguments = $"/n, /select, {path}" });
+                        }
+                        catch (Exception)
+                        {
+
+                            await client.SendTextMessageAsync(message.Chat.Id, "Не удалось");
+                        }
+                        
+                        Console.WriteLine("OK");
+                        com = "";
+                    }
                 }
-                await client.SendTextMessageAsync(message.Chat.Id, "Сообщение");
+                
             }
         }
+
+    
 
         private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
         {
